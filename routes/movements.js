fa35536cb25c2cf0c1bfb6d01351ffb2c22f575a
Sherwin <sherwin.kwan92@gmail.com>
2021-01-15 17:01:24 -0800

    add get movement and delete routes

--- routes/movements.js
+++ routes/movements.js
@@ -4,9 +4,10 @@ const movements = require('../data/movements');
 
 /* GET a single movement */
 router.get('/:id', function(req, res, next) {
-  res.json();
+  const myMovement = movements.filter(movement => movement.id === Number(req.params.id));
+  console.log('Filtered: ', myMovement, ' , ', req.params.id);
+  res.json(myMovement);
 });
-
 
 /* EDIT a movement */
 router.post('/:id', (req, res, next) => {
@@ -15,7 +16,12 @@ router.post('/:id', (req, res, next) => {
 
 /* DELETE a movement */
 router.delete('/:id', (req, res, next) => {
-
+  const myId = movements.indexOf(movement => movement.id === Number(req.params.id));
+  if (myId >= 0) {
+    movements.splice(myId, 1);
+  } else {
+    res.status(404).send('The movement to be deleted cannot be found');
+  }
 });
 
 /* GET all movements */
