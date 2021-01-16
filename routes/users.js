6035045792223e8e70075c93aac4df9ae1ff5e8f
Sherwin <sherwin.kwan92@gmail.com>
2021-01-15 16:41:20 -0800

    create basic REST API routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
+/* GET a single movement */
+router.get('/:id', function(req, res, next) {
+  res.json();
+});
+
+
+/* EDIT a movement */
+router.post('/:id', (req, res, next) => {
+
+});
+
+/* DELETE a movement */
+router.delete('/:id', (req, res, next) => {
+  
+});
+
+/* GET all movements */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json();
+});
+
+
+/* CREATE new movement */
+router.post('/', (req, res, next) => {
+
 });
 
 module.exports = router;
