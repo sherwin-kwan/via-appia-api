1cfec49ec0e1a5024879808238c07997a4f76a92
Sherwin <sherwin.kwan92@gmail.com>
2021-01-22 20:07:30 -0800

    add endpoints to create and edit movements

--- routes/movements.js
+++ routes/movements.js
@@ -5,21 +5,22 @@ const movements = require('../data/movements');
 /* GET a single movement */
 router.get('/:id', function(req, res, next) {
   const myMovement = movements.filter(movement => movement.id === Number(req.params.id));
-  console.log('Filtered: ', myMovement, ' , ', req.params.id);
   res.json(myMovement);
 });
 
 /* EDIT a movement */
 router.post('/:id', (req, res, next) => {
-  
+  const index = movements.findIndex(movement => movement.id === Number(req.params.id));
+  movements[index] = req.body.data;
+  res.send("Done");
 });
 
 /* DELETE a movement */
 router.delete('/:id', (req, res, next) => {
   const myId = movements.findIndex(movement => movement.id === Number(req.params.id));
-  console.log('myId is: ', myId, 'since the param is: ', req.params.id);
   if (myId >= 0) {
     movements.splice(myId, 1);
+    res.status(204).send("Deletion successful");
   } else {
     res.status(404).send('The movement to be deleted cannot be found');
   }
@@ -33,7 +34,10 @@ router.get('/', function(req, res, next) {
 
 /* CREATE new movement */
 router.post('/', (req, res, next) => {
-
+  const nextId = movements[movements.length - 1].id + 1;
+  const newMovement = Object.assign(req.body.data, {id: nextId});
+  movements.push(newMovement);
+  res.send("Done");
 });
 
 module.exports = router;
