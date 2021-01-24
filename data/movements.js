84c5a367413e0798ca98a06849efd9956f9771b6
Sherwin <sherwin.kwan92@gmail.com>
2021-01-24 13:39:41 -0800

    add more movements

--- data/movements.js
+++ data/movements.js
@@ -20,6 +20,28 @@ const movements = [
     "endLocation": "Vancouver, BC",
     "freight": "2 crates of Pfizer vaccine",
     "details": "Ensure freight is chilled at -70 degrees"
+  },
+  {
+    "id": 3,
+    "startLat": 37.33,
+    "startLong": -122.03,
+    "endLat": 47.62,
+    "endLong": -122.34,
+    "startLocation": "Cupertino, CA",
+    "endLocation": "Seattle, WA",
+    "freight": "100 brand new iPads",
+    "details": "Make sure to say hello to Mr. Bezos for me!"
+  },
+  {
+    "id": 4,
+    "startLat": 37.33,
+    "startLong": -122.03,
+    "endLat": 34.13,
+    "endLong": -118.32,
+    "startLocation": "Mount St. Helens, WA",
+    "endLocation": "Los Angeles, CA",
+    "freight": "Volcanic ash souvenirs",
+    "details": "Feel free to cancel this delivery if the volcano is actively spewing smoke"
   }
 ];
 
