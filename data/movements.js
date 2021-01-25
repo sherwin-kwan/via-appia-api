c3c75769f79e7094de7802a88af540aed1f062e4
Sherwin <sherwin.kwan92@gmail.com>
2021-01-24 18:20:33 -0800

    mix up the routes

--- data/movements.js
+++ data/movements.js
@@ -1,47 +1,58 @@
 const movements = [
   {
     "id": 1,
-    "startLat": 49.25,
-    "startLong": -123,
-    "endLat": 51.05,
-    "endLong": -114.05,
-    "startLocation": "Vancouver, BC",
-    "endLocation": "Calgary, AB",
-    "freight": "2 crates of Pfizer vaccine",
-    "details": "Ensure freight is chilled at -70 degrees"
+    "startLat": 34.13,
+    "startLong": -118.32,
+    "endLat": 60.7,
+    "endLong": -135.05,
+    "startLocation": "Los Angeles, CA",
+    "endLocation": "Whitehorse, YK",
+    "freight": "2 crates of Pfizer-BioNTech vaccine",
+    "details": "Ensure freight is chilled at -70 degrees."
   },
   {
     "id": 2,
-    "startLat": 49.9,
-    "startLong": -119.45,
+    "startLat": 42.2,
+    "startLong": -71.2,
     "endLat": 49.25,
     "endLong": -123,
-    "startLocation": "Kelowna, BC",
+    "startLocation": "Moderna Corp, Norwood, MA",
     "endLocation": "Vancouver, BC",
-    "freight": "2 crates of Pfizer vaccine",
-    "details": "Ensure freight is chilled at -70 degrees"
+    "freight": "2 crates of Moderna mRNA-1273 vaccine",
+    "details": "Ensure freight is chilled at -20 degrees"
   },
   {
     "id": 3,
-    "startLat": 37.33,
-    "startLong": -122.03,
-    "endLat": 47.62,
-    "endLong": -122.34,
-    "startLocation": "Cupertino, CA",
-    "endLocation": "Seattle, WA",
-    "freight": "100 brand new iPads",
-    "details": "Make sure to say hello to Mr. Bezos for me!"
+    "startLat": 47.62,
+    "startLong": -122.34,
+    "endLat": 37.33,
+    "endLong": -122.03,
+    "startLocation": "Amazon Inc, Seattle, WA",
+    "endLocation": "Apple Inc, Cupertino, CA",
+    "freight": "100 Kindles",
+    "details": "Please say hello to Mr. Cook for me, and remind him to pay the $100 million invoice for using too much AWS!"
   },
   {
     "id": 4,
-    "startLat": 37.33,
-    "startLong": -122.03,
+    "startLat": 46.28,
+    "startLong": -122.22,
     "endLat": 34.13,
     "endLong": -118.32,
     "startLocation": "Mount St. Helens, WA",
     "endLocation": "Los Angeles, CA",
     "freight": "Volcanic ash souvenirs",
     "details": "Feel free to cancel this delivery if the volcano is actively spewing smoke"
+  },
+  {
+    "id": 5,
+    "startLat": 43.65,
+    "startLong": -79.37,
+    "endLat": 49.25,
+    "endLong": -123,
+    "startLocation": "Rose Rocket, Toronto, ON",
+    "endLocation": "Vancouver, BC",
+    "freight": "Invitation letter to a job interview",
+    "details": "Candidate performance to be assessed based on a coding exercise, answers to behavioural and technical questions, and culture fit. Job offer may follow."
   }
 ];
 
